e93a4f27be553241f8949f47895779fcff87a8aa
Denys Sobchuk <d.sobchuk@perenio.com>
2024-03-06 06:24:10 +0000

    #define LWIP_NO_UNISTD_H                        1

--- src/include/lwipopts.h
+++ src/include/lwipopts.h
@@ -3,6 +3,7 @@
 
 #define NO_SYS                  		1
 #define SYS_LIGHTWEIGHT_PROT    		0
+#define LWIP_NO_UNISTD_H                        1
 
 #define LWIP_NETCONN                    0
 #define LWIP_SOCKET                     0
